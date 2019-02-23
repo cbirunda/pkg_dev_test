5a172dc74885c00759cbc57b136447c1ea3fd892
Birunda Chelliah <cbirunda@gmail.com>
2019-02-23 14:31:05 -0600

    Modified dedup function, namespace file, description file.

--- R/dedup.R
+++ R/dedup.R
@@ -1,3 +1,4 @@
+
 #' Title
 #'
 #' @param df
@@ -7,13 +8,14 @@
 #' @export
 #'
 #' @examples
+#'
+#'
 dedup<-function(df, group_var){
   group_var<-dplyr::enquo(group_var)
 
-  df%<>%
+  df<-df%>%
     dplyr::group_by(!!group_var)%>%
-    dplyr::mutate(dup=n(),
-                  rank=row_number(!!group_var))%>%
+    dplyr::mutate(rank=dplyr::row_number(!!group_var))%>%
     dplyr::filter(rank==1)%>%
-    dplyr::select(-dup, -rank)
+    dplyr::select(-rank)
 }
